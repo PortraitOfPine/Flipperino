1ae41fd4a0d16a990b8587628736bc1a76641693
Bruno Roy <brusi_roy@hotmail.com>
2018-12-24 18:44:22 -0500

    Add camera movement
    The camera is now following `me_` in the x axis.

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+    #region fields
+    [SerializeField]
+    private GameObject followedTarget_;
+    #endregion
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3(this.followedTarget_.transform.position.x, 0.0f, -10.0f);
+    }
+}
