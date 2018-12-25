9c8678191284c46ed2dcb8b237464411164b23b1
Bruno Roy <brusi_roy@hotmail.com>
2018-12-24 20:17:45 -0500

    Add score display, top right corner

---
+++ Assets/Scripts/ScoreCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class ScoreCalculator : MonoBehaviour
+{
+
+    #region fields
+    [SerializeField]
+    private Text textScore_;
+    private int score_;
+    #endregion
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.score_ += 1;
+        this.textScore_.text = this.score_.ToString();
+    }
+}
